6121ef5c4bdc39d6ca21822cc3e7cd4c697cd472
Davy De Waele <ddewaele@gmail.com>
2023-11-24 23:22:57 +0100

    initial commit

--- lib/cdk-simple-cognito-stack.ts
+++ lib/cdk-simple-cognito-stack.ts
@@ -1,16 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { OAuthScope, UserPool, VerificationEmailStyle } from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkSimpleCognitoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkSimpleCognitoStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a new Cognito User Pool
+    const userPool = new UserPool(this, 'UserPool', {
+      // User Pool configuration here
+      selfSignUpEnabled: true,
+      userVerification: {
+        emailSubject: 'Verify your email for our awesome app!',
+        emailBody: 'Thanks for signing up to our awesome app! Your verification code is {####}',
+        emailStyle: VerificationEmailStyle.CODE,
+      },
+      signInAliases: {
+        email: true,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkSimpleCognitoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // Create App Client for Authorization Code Flow
+    const authCodeClient = userPool.addClient('AuthCodeClient', {
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true,
+        },
+        scopes: [OAuthScope.OPENID, OAuthScope.EMAIL, OAuthScope.PROFILE],
+        callbackUrls: ['http://localhost:8080/login/oauth2/code/cognito'],
+      },
+    });
+
+    // // Create App Client for Client Credentials Flow
+    // const clientCredentialsClient = userPool.addClient('ClientCredentialsClient', {
+    //   generateSecret: true, // Needed for client credentials flow
+    //   oAuth: {
+    //     flows: {
+    //       clientCredentials: true,
+    //     },
+    //     scopes: [OAuthScope.COGNITO_ADMIN],
+    //   },
     // });
+
+    // Output the IDs of the created resources
+    new cdk.CfnOutput(this, 'UserPoolId', { value: userPool.userPoolId });
+    new cdk.CfnOutput(this, 'AuthCodeClientId', { value: authCodeClient.userPoolClientId });
+    //new cdk.CfnOutput(this, 'ClientCredentialsClientId', { value: clientCredentialsClient.userPoolClientId });
   }
 }
